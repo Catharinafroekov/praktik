784997b5635595bb4881b3baca0aee5739d36872
Catharina <catharina2002@gmail.com>
2024-11-07 21:06:12 +0100

    fjern midlertidig nav

--- nav.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu_button');
-const menu = document.querySelector('.nav-bar ul');
-
-menuButton.addEventListener('click', () => {
-    menu.classList.toggle('open');
-    menuButton.classList.toggle('open');
-});
